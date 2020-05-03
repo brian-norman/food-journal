d1f6cc9a18335ac5004507d60e57b4b131d43440
Brian Norman <b2norman@uwaterloo.ca>
2020-05-03 08:43:24 -0600

    Remove unused ViewModel

--- app/src/main/java/com/example/foodjournal/MainViewModel.kt
+++
@@ -1,9 +0,0 @@
-package com.example.foodjournal
-
-import androidx.compose.Model
-import com.example.foodjournal.data.fakeEntries
-import com.example.foodjournal.model.Entry
-
-class MainViewModel {
-
-}
